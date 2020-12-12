54fd8b6a7d60b8b7df3e64603608aaf7f01b0ca0
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2020-12-13 01:25:43 +0300

    add validation

--- app/job_seeker/routes.py
+++ app/job_seeker/routes.py
@@ -1,6 +1,20 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
+from app.job_seeker.forms import FillInForm
+from app.scraping.scraper import get_records
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    form = FillInForm()
+    if form.validate_on_submit():
+        position = form.position.data
+        location = form.location.data
+        get_records(position, location)
+        return redirect(url_for("results"))
+    return render_template("index.html", form=form)
+
+
+@app.route("/results", methods=["GET", "POST"])
+def results():
+    pass
