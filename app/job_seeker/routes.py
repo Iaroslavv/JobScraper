27ae059c9daa5b42de16773fff4f87b406de231c
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2020-12-13 18:32:51 +0300

    add selection

--- app/job_seeker/routes.py
+++ app/job_seeker/routes.py
@@ -1,20 +1,27 @@
-from app import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, Blueprint, request
 from app.job_seeker.forms import FillInForm
 from app.scraping.scraper import get_records
 
 
-@app.route("/", methods=["GET", "POST"])
+users = Blueprint("users", __name__)
+
+
+@users.route("/", methods=["GET", "POST"])
 def index():
     form = FillInForm()
-    if form.validate_on_submit():
-        position = form.position.data
-        location = form.location.data
-        get_records(position, location)
-        return redirect(url_for("results"))
+    site = request.form.get("websites")
+    if request.method == "POST":
+        if site == "Indeed.com":
+            position = form.position.data
+            location = form.location.data
+            get_records(position, location)
+            print("DONE!")
+            return redirect(url_for("users.results"))
+        else:
+            print("NOT INDEED")
     return render_template("index.html", form=form)
 
 
-@app.route("/results", methods=["GET", "POST"])
+@users.route("/results", methods=["GET"])
 def results():
-    pass
+    return render_template("results.html")
